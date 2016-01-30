04236b2ab5a9c8f9d86bf64b5927ec1572f86d31
James P. Howard, II <jh@jameshoward.us>
2016-01-30 15:30:42 -0500

    Added package-level documentation

---
+++ R/cmna.R
@@ -0,0 +1,20 @@
+#' @title Computational Methods for Numerical Analysis
+#' @docType package
+#' 
+#' @description
+#' Provides the source and examples for \emph{Computational Methods for
+#' Numerical Analysis with R}.
+#'
+#' @details
+#' This package provides a suite of simple implementations of standard
+#' methods from numerical analysis.  The collection is designed to
+#' accompany \emph{Computational Methods for Numerical Analysis with R}
+#' by James P. Howard, II.  Together, these functions provide methods to
+#' support linear algebra, interpolation, integration, root finding,
+#' optimization, and differential equations.
+#'
+#' @author
+#' James P. Howard, II <jh@jameshoward.us>
+#' 
+"_PACKAGE"
+#> [1] "_PACKAGE"
