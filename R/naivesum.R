36eb9b58c4837e07ba6fb01d1aa924bd9448edd0
James P. Howard, II <jh@jameshoward.us>
2016-07-05 16:43:28 -0400

    Swapping uppercase/lowercase l/L per reviewer, for typography

--- R/naivesum.R
+++ R/naivesum.R
@@ -49,12 +49,12 @@
 #' @family intro
 #'
 #' @examples
-#' l <- 1:10^6
-#' n <- sample(l, 1)
-#' bound <- sample(l, 2)
-#' bound.u <- max(bound) - 10^6 / 2
-#' bound.l <- min(bound) - 10^6 / 2
-#' x <- runif(n, bound.l, bound.u)
+#' k <- 1:10^6
+#' n <- sample(k, 1)
+#' bound <- sample(k, 2)
+#' bound.upper <- max(bound) - 10^6 / 2
+#' bound.lower <- min(bound) - 10^6 / 2
+#' x <- runif(n, bound.lower, bound.upper)
 #' naivesum(x)
 #' kahansum(x)
 #' pwisesum(x)
